8da33eee745e8d0a549e26bc8c6658fa00accfd2
Ryan Gross <ryangross47@gmail.com>
2025-04-23 18:51:12 -0400

    Add Line and Triangle

--- parser/src/main/java/less/lgeo/primitive/Line.java
+++ parser/src/main/java/less/lgeo/primitive/Line.java
@@ -1,4 +1,24 @@
 package less.lgeo.primitive;
 
 public class Line {
+
+  private static final LineType type = LineType.LINE;
+
+  private final Color color;
+  private final int x1;
+  private final int y1;
+  private final int z1;
+  private final int x2;
+  private final int y2;
+  private final int z2;
+
+  public Line(Color color, int x1, int y1, int z1, int x2, int y2, int z2) {
+    this.color = color;
+    this.x1 = x1;
+    this.y1 = y1;
+    this.z1 = z1;
+    this.x2 = x2;
+    this.y2 = y2;
+    this.z2 = z2;
+  }
 }
