e7231c93a126a709eb64e099ad3cbd4931122123
Ryan Gross <ryangross47@gmail.com>
2025-05-10 13:14:34 -0400

    trying to test logging but my pc sucks

--- parser/src/main/java/less/lgeo/ParserHandler.java
+++ parser/src/main/java/less/lgeo/ParserHandler.java
@@ -6,7 +6,9 @@ import less.lgeo.parse.Parser;
 import less.lgeo.set.Model;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class ParserHandler {
 
   private static final Logger logger = LoggerFactory.getLogger(ParserHandler.class);
