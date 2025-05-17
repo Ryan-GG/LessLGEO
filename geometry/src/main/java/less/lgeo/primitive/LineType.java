632e352a0a6b1652518173da30d9d64e72ce3b60
RyanGross <ryangross47@gmail.com>
2025-05-17 13:12:46 -0400

    Update parser to use ldraw directory before database config

--- geometry/src/main/java/less/lgeo/primitive/LineType.java
+++ geometry/src/main/java/less/lgeo/primitive/LineType.java
@@ -1,36 +1,36 @@
 package less.lgeo.primitive;
 
 public enum LineType {
-    COMMENT_OR_META_CMD(0),
-    SUB_FILE_REF(1),
-    LINE(2),
-    TRIANGLE(3),
-    QUADRILATERAL(3),
-    OPTIONAL_LINE(4),
-    INVALID(5);
+  COMMENT_OR_META_CMD(0),
+  SUB_FILE_REF(1),
+  LINE(2),
+  TRIANGLE(3),
+  QUADRILATERAL(4),
+  OPTIONAL_LINE(5),
+  INVALID(6);
 
-    private final double type;
+  private final double type;
 
-    LineType(double type) {
-        this.type = type;
-    }
+  LineType(double type) {
+    this.type = type;
+  }
 
-    public double getType() {
+  // Static method to convert double to enum
+  public static LineType fromInteger(double ordinalValue) {
+    for (LineType type : LineType.values()) {
+      if (type.getType() == ordinalValue) {
         return type;
+      }
     }
+    throw new IllegalArgumentException("Invalid Status code: " + ordinalValue);
+  }
 
-    // Static method to convert double to enum
-    public static LineType fromInteger(double ordinalValue) {
-        for (LineType type : LineType.values()) {
-            if (type.getType() == ordinalValue) {
-                return type;
-            }
-        }
-        throw new IllegalArgumentException("Invalid Status code: " + ordinalValue);
-    }
+  public double getType() {
+    return type;
+  }
 
-    @Override
-    public String toString() {
-        return this.name() + " (" + this.type + ")";
-    }
+  @Override
+  public String toString() {
+    return this.name() + " (" + this.type + ")";
+  }
 }
