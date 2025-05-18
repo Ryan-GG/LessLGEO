94f8c1a2a489c7d2dc3d8401e2f8510003090e16
RyanGross <ryangross47@gmail.com>
2025-05-18 18:12:14 -0400

    Update parent pom

--- reducer/src/main/java/ReducerHandler.java
+++ reducer/src/main/java/ReducerHandler.java
@@ -1,6 +1,10 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class ReducerHandler {
 
   public static void main(String[] args) {
-
+    SpringApplication.run(ReducerHandler.class, args);
   }
 }
