3f04f904ca02230a97747b34da68b67a05f35c15
RyanGross <ryangross47@gmail.com>
2025-06-01 17:52:34 -0400

    Rename module and add Connecitivy parser stub

---
+++ parser/src/main/java/less/lgeo/parse/ConnectivityParser.java
@@ -0,0 +1,20 @@
+package less.lgeo.parse;
+
+import java.io.File;
+import java.io.IOException;
+import less.lgeo.connection.Connection;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ConnectivityParser implements Parser<Connection> {
+
+  @Override
+  public Connection parse( File fileToParse ) throws IOException {
+    return null;
+  }
+
+  @Override
+  public File writeToFile( Connection gpb, String fileName ) {
+    return null;
+  }
+}
