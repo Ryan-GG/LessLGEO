91b3c45e4733caeba365181aab3d1052940b83e1
Ryan Gross <ryangross47@gmail.com>
2025-06-22 16:30:07 -0400

    Redesign of producer / consumer

--- reducer/src/main/java/less/lgeo/ReducerHandler.java
+++ reducer/src/main/java/less/lgeo/ReducerHandler.java
@@ -1,31 +1,32 @@
 package less.lgeo;
 
+import less.lgeo.primitive.Model;
 import less.lgeo.rabbitmq.RabbitProperties;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.ApplicationArguments;
-import org.springframework.boot.ApplicationRunner;
-import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
-@EnableConfigurationProperties(value = RabbitProperties.class)
 @SpringBootApplication
-public class ReducerHandler implements ApplicationRunner {
+@EnableConfigurationProperties(value = {RabbitProperties.class})
+public class ReducerHandler {
 
   private static final Logger logger = LoggerFactory.getLogger(ReducerHandler.class);
 
   public static void main(String[] args) {
+/*
     new SpringApplicationBuilder()
         .web(WebApplicationType.NONE)
         .sources(ReducerHandler.class)
         .build()
         .run(args);
+*/
+    SpringApplication.run(ReducerHandler.class);
   }
 
 
-  @Override
-  public void run(ApplicationArguments args) {
+  public void consume(Model message) {
+    logger.info("foo {}", message);
   }
 }
--- reducer/src/main/java/less/lgeo/consumer/ReducerConsumer.java
+++ reducer/src/main/java/less/lgeo/consumer/ReducerConsumer.java
@@ -1,5 +1,6 @@
 package less.lgeo.consumer;
 
+import less.lgeo.ReducerHandler;
 import less.lgeo.primitive.Model;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,10 +12,15 @@ import org.springframework.stereotype.Component;
 public class ReducerConsumer {
 
   private static final Logger logger = LoggerFactory.getLogger(ReducerConsumer.class);
+  private final ReducerHandler reducerHandler;
+
+  public ReducerConsumer(ReducerHandler reducerHandler) {
+    this.reducerHandler = reducerHandler;
+  }
 
   @RabbitListener(queues = "parser-to-reducer-queue")
   public void handleMessage(@Payload Model message) {
-    logger.info("Received Model: \n {}", message.toString());
+    reducerHandler.consume(message);
   }
 
 }
