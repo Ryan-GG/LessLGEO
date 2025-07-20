88b47f614827f7ee54112aa77f8880cea7255c60
RyanGross <ryangross47@gmail.com>
2025-07-20 19:05:53 -0400

    Remove profile for bean creation

--- parser/src/main/java/less/lgeo/config/ParserRabbitConfig.java
+++ parser/src/main/java/less/lgeo/config/ParserRabbitConfig.java
@@ -5,19 +5,14 @@ import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 
 @EnableRabbit
 @Configuration
 class ParserRabbitConfig {
 
-  @Profile("lDrawParser")
-  private static class LDrawParserConfig {
-
-    @Bean
-    Queue webQueueToParser(RabbitWorkerQueueProperties rabbitWorkerQueueProperties) {
-      return new Queue(rabbitWorkerQueueProperties.getWebToParser(), false);
-    }
+  @Bean
+  Queue webQueueToParser( RabbitWorkerQueueProperties rabbitWorkerQueueProperties ) {
+    return new Queue( rabbitWorkerQueueProperties.getWebToParser(), false );
   }
 
 }
