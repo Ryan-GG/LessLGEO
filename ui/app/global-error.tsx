1fcf3276602534582c53ec56325168401f28eff4
Ryan Gross <ryangross47@gmail.com>
2025-07-24 20:56:16 -0400

    Task: Add boundary files

---
+++ ui/app/global-error.tsx
@@ -0,0 +1,6 @@
+export default function GlobalError()
+{
+    return( 
+        <div>TODO, This is a global error</div>
+    );
+}
---
+++ ui/app/not-found.tsx
@@ -0,0 +1,5 @@
+export default function NotFound() {
+  return (
+    <div>TODO, URL Not found</div>
+  );
+}
