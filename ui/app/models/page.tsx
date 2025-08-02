d19b324bc5186eb0a64bff894edf57d4b5735279
RyanGross <ryangross47@gmail.com>
2025-08-02 15:34:45 -0400

    bug: fetch ids on page but need to fix CORS on spring backend

--- ui/app/models/page.tsx
+++ ui/app/models/page.tsx
@@ -2,12 +2,23 @@
 import { AppNavBar } from "@/components/home/app-nav-bar";
 import { ModelCanvas } from "@/components/models/model-canvas";
 import { Skeleton } from "@/components/ui/skeleton";
+import { useEffect, useState } from "react";
 
 /**
  * Models Landing Page, where to view and add new Models
- * TODO,
  */
 export default function Models() {
+
+	const [data, setData] = useState<Array<String>>(["foop"]);  
+
+	useEffect(() => {
+	  fetch('http://localhost:8080/api/model/v1/ids')
+		.then((response) => response.json())
+		.then((data) => {
+		  setData(data);
+		});
+	}, []);
+
 	return (
 		<div className="flex flex-col min-h-screen bg-black">
 			<header className="px-8">
@@ -16,7 +27,15 @@ export default function Models() {
 			<main className="flex-1 pb-8 px-8 pt-2">
 				<div className="h-full w-full p-8 flex flex-col gap-4 bg-gray-200">
 					<div className="flex gap-4">
-						<Skeleton className="h-[10vh] w-1/3 bg-black"/>
+						<div className="h-[10vh] w-1/3">
+							{data.map( id => {
+								return ( 
+								<p key={`id-${id}`}>
+									{`ID: ${id}`}
+								</p> 
+								);
+							})}
+						</div>
 					</div>
 					<div className="flex gap-4">
 						<div className="h-[70vh] w-2/3 border-2 border-gray-500 rounded-lg p-6" id="canvas-container">
--- web-server/src/main/java/less/lgeo/controller/ModelController.java
+++ web-server/src/main/java/less/lgeo/controller/ModelController.java
@@ -12,6 +12,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -61,6 +62,8 @@ public class ModelController {
     }
   }
 
+  //TODO, fix CORS issue
+  @CrossOrigin( origins = "http://localhost:3000" )
   @GetMapping( "/ids" )
   public ResponseEntity<List<UUID>> getAllModelIds() {
     return ResponseEntity.ok( modelService.getAllModelUUIDs() );
