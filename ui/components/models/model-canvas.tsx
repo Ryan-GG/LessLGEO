d1e49be2ff8b20989090749175c4cb239913e8a4
RyanGross <ryangross47@gmail.com>
2025-08-10 20:44:43 -0400

    Refactor: Remove dead code

--- ui/components/models/model-canvas.tsx
+++ ui/components/models/model-canvas.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { CameraControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
-import { ReactNode } from "react";
+import { ReactNode, useEffect } from "react";
 import { Model } from "./primitive/model";
 import { fetchModelById } from "@/api/modelApi";
 import { useQuery } from "@tanstack/react-query";
@@ -13,14 +13,15 @@ export const DEFAULT_UP = new Vector3( 0, -1, 0 );
 
 export function ModelCanvas( { modelId }: {modelId: string | undefined} ): ReactNode
 {
+	Object3D.DEFAULT_UP = DEFAULT_UP;
+
 	const { data: model } = useQuery( { queryKey: [ "model" ], 
 		enabled: modelId !== undefined,
 		queryFn: () => fetchModelById( modelId! ) } );
 
-	Object3D.DEFAULT_UP = DEFAULT_UP;
-
+	
 	return (
-		<Canvas camera={{ position: [ 0, -500, -500 ], far: 5000 }} gl={{}}>
+		<Canvas camera={{ position: [ 0, -500, -500 ], far: 5000 }}>
 			<Model gpb={model}/>
 			<GridHelper/>
 			<ambientLight intensity={0.1} />
