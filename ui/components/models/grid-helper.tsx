2350aeb0da54d7af378f353d83fb706528774d3e
RyanGross <ryangross47@gmail.com>
2025-08-24 17:46:46 -0400

    Refactor: Fix constructors and color so its respective to the axis

--- ui/components/models/grid-helper.tsx
+++ ui/components/models/grid-helper.tsx
@@ -1,12 +1,14 @@
 import { THREE_LDU_SCALAR_VECTOR } from "@/utils/units-utilities";
 import { Text3D } from "@react-three/drei";
 import { ReactElement, Suspense } from "react";
-import { Color, ColorRepresentation }  from "three";
+import { Color, MeshStandardMaterial }  from "three";
 
 export const GRID_SIZE: number = 50 as const;
+export const GRID_COLOR_CENTER: Color = new Color(1,0,0);
+export const GRID_COLOR_LINES: Color = new Color(0.2,0.2, 0.2);
 
-export const GRID_COLOR_CENTER: ColorRepresentation = new Color().setRGB( 1, 0, 0 );
-export const GRID_COLOR_LINES: ColorRepresentation = new Color().setRGB( 0.2, 0.2, 0.2 );
+export const X_COLOR: Color = new Color(1,0,0);
+export const Z_COLOR: Color = new Color(0,0,1);
 
 export function GridHelper(): ReactElement
 {
@@ -18,18 +20,11 @@ export function GridHelper(): ReactElement
 					size={THREE_LDU_SCALAR_VECTOR.x}
 					height={1}
 					curveSegments={12}
-					bevelEnabled
-					bevelThickness={0.03}
-					bevelSize={0.02}
-					bevelOffset={0}
-					bevelSegments={5}
 					rotation={[ 0, Math.PI, Math.PI ]}
-					position={[ ( ( GRID_SIZE * THREE_LDU_SCALAR_VECTOR.x ) / 2 ),
-						0,
-						0 ]}
+					position={[ ( ( GRID_SIZE * THREE_LDU_SCALAR_VECTOR.x ) / 2 ), 0, 0 ]}
 				>
 					{`X: ${( GRID_SIZE ) / 2}`}
-					<meshStandardMaterial color="orange" />
+					<meshStandardMaterial color={X_COLOR} />
 				</Text3D>
 			</Suspense>
 			<Suspense fallback={undefined}>
@@ -38,22 +33,14 @@ export function GridHelper(): ReactElement
 					size={THREE_LDU_SCALAR_VECTOR.z}
 					height={1}
 					curveSegments={12}
-					bevelEnabled
-					bevelThickness={0.03}
-					bevelSize={0.02}
-					bevelOffset={0}
-					bevelSegments={5}
 					rotation={[ 0, Math.PI, Math.PI ]}
-					position={[ 0,
-						0,
-						( ( GRID_SIZE * THREE_LDU_SCALAR_VECTOR.z ) / 2 ), ]}
+					position={[ 0, 0, ( ( GRID_SIZE * THREE_LDU_SCALAR_VECTOR.z ) / 2 ) ]}
 				>
 					{`Z: ${( GRID_SIZE ) / 2}`}
-					<meshStandardMaterial color="orange" />
+					<meshStandardMaterial color={Z_COLOR} />
 				</Text3D>
 			</Suspense>
 			<gridHelper args={[ GRID_SIZE, GRID_SIZE, GRID_COLOR_CENTER, GRID_COLOR_LINES ]} scale={THREE_LDU_SCALAR_VECTOR}/>
 		</group>
-        
 	);
 }
