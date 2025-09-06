ee0faa76eec9861b0402e5f4886a4357f3ba256c
Ryan Gross <ryangross47@gmail.com>
2025-09-06 16:57:15 -0400

    Task & Refactor: Naming for some volumes, and now failed messages get routed to the respective Dead-Letter-Queue

--- parser/src/main/java/less/lgeo/config/RabbitConfig.java
+++ parser/src/main/java/less/lgeo/config/RabbitConfig.java
@@ -3,7 +3,11 @@ package less.lgeo.config;
 import less.lgeo.rabbitmq.RabbitDeadLetterProperties;
 import less.lgeo.rabbitmq.RabbitExchangeProperties;
 import less.lgeo.rabbitmq.RabbitWorkerQueueProperties;
-import org.springframework.amqp.core.*;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.QueueBuilder;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,77 +16,81 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 class RabbitConfig {
 
-    @Bean
-    Queue webToParserQueue(
-            RabbitWorkerQueueProperties rabbitWorkerQueueProperties,
-            RabbitDeadLetterProperties rabbitDeadLetterProperties) {
-        return QueueBuilder.durable(rabbitWorkerQueueProperties.getWebToParser())
-                .deadLetterExchange(rabbitDeadLetterProperties.getDeadLetterExchange())
-                .deadLetterRoutingKey(rabbitDeadLetterProperties.getWebToParserRoutingKey())
-                .build();
-    }
+  @Bean
+  Queue webToParserQueue(
+      RabbitWorkerQueueProperties rabbitWorkerQueueProperties,
+      RabbitDeadLetterProperties rabbitDeadLetterProperties) {
+    return QueueBuilder.durable(rabbitWorkerQueueProperties.getWebToParser())
+        .deadLetterExchange(rabbitDeadLetterProperties.getDeadLetterExchange())
+        .deadLetterRoutingKey(rabbitDeadLetterProperties.getWebToParserRoutingKey())
+        .build();
+  }
 
-    @Bean
-    public DirectExchange webToParserExchange(RabbitExchangeProperties rabbitExchangeProperties) {
-        return new DirectExchange(rabbitExchangeProperties.getWebToParserExchange());
-    }
+  @Bean
+  public DirectExchange webToParserExchange(RabbitExchangeProperties rabbitExchangeProperties) {
+    return new DirectExchange(rabbitExchangeProperties.getWebToParserExchange());
+  }
 
-    @Bean
-    public FanoutExchange deadLetterExchange(RabbitDeadLetterProperties rabbitDeadLetterProperties) {
-        return new FanoutExchange(rabbitDeadLetterProperties.getDeadLetterExchange());
-    }
+  @Bean
+  public DirectExchange deadLetterExchange(RabbitDeadLetterProperties rabbitDeadLetterProperties) {
+    return new DirectExchange(rabbitDeadLetterProperties.getDeadLetterExchange());
+  }
 
-    @Bean
-    public Binding webToParserBinding(
-            Queue webToParserQueue,
-            DirectExchange webToParserExchange) {
-        return BindingBuilder.bind(webToParserQueue).to(webToParserExchange).withQueueName();
-    }
+  @Bean
+  public Binding webToParserBinding(
+      Queue webToParserQueue,
+      DirectExchange webToParserExchange) {
+    return BindingBuilder.bind(webToParserQueue).to(webToParserExchange).withQueueName();
+  }
 
-    @Bean
-    Queue parserToReducerQueue(
-            RabbitWorkerQueueProperties rabbitWorkerQueueProperties,
-            RabbitDeadLetterProperties rabbitDeadLetterProperties) {
-        return QueueBuilder.durable(rabbitWorkerQueueProperties.getParserToReducer())
-                .deadLetterExchange(rabbitDeadLetterProperties.getDeadLetterExchange())
-                .deadLetterRoutingKey(rabbitDeadLetterProperties.getParserToReducerRoutingKey())
-                .build();
-    }
+  @Bean
+  Queue parserToReducerQueue(
+      RabbitWorkerQueueProperties rabbitWorkerQueueProperties,
+      RabbitDeadLetterProperties rabbitDeadLetterProperties) {
+    return QueueBuilder.durable(rabbitWorkerQueueProperties.getParserToReducer())
+        .deadLetterExchange(rabbitDeadLetterProperties.getDeadLetterExchange())
+        .deadLetterRoutingKey(rabbitDeadLetterProperties.getParserToReducerRoutingKey())
+        .build();
+  }
 
-    @Bean
-    Queue webToParserDeadLetterQueue(RabbitDeadLetterProperties rabbitDeadLetterProperties) {
-        return new Queue(rabbitDeadLetterProperties.getWebToParserDeadLetterQueue(), true);
-    }
+  @Bean
+  Queue webToParserDeadLetterQueue(RabbitDeadLetterProperties rabbitDeadLetterProperties) {
+    return new Queue(rabbitDeadLetterProperties.getWebToParserDeadLetterQueue(), true);
+  }
 
-    @Bean
-    Queue parserToReducerDeadLetterQueue(RabbitDeadLetterProperties rabbitDeadLetterProperties) {
-        return new Queue(rabbitDeadLetterProperties.getParserToReducerDeadLetterQueue(), true);
-    }
+  @Bean
+  Queue parserToReducerDeadLetterQueue(RabbitDeadLetterProperties rabbitDeadLetterProperties) {
+    return new Queue(rabbitDeadLetterProperties.getParserToReducerDeadLetterQueue(), true);
+  }
 
-    @Bean
-    public DirectExchange parserToReducerExchange(RabbitExchangeProperties rabbitExchangeProperties) {
-        return new DirectExchange(rabbitExchangeProperties.getParserToReducerExchange());
-    }
+  @Bean
+  public DirectExchange parserToReducerExchange(RabbitExchangeProperties rabbitExchangeProperties) {
+    return new DirectExchange(rabbitExchangeProperties.getParserToReducerExchange());
+  }
 
-    @Bean
-    public Binding parserToReducerBinding(
-            Queue parserToReducerQueue,
-            DirectExchange parserToReducerExchange) {
-        return BindingBuilder.bind(parserToReducerQueue).to(parserToReducerExchange).withQueueName();
-    }
+  @Bean
+  public Binding parserToReducerBinding(
+      Queue parserToReducerQueue,
+      DirectExchange parserToReducerExchange) {
+    return BindingBuilder.bind(parserToReducerQueue).to(parserToReducerExchange).withQueueName();
+  }
 
-    @Bean
-    public Binding parserToReduceDeadLetterBinding(
-            Queue parserToReducerDeadLetterQueue,
-            FanoutExchange deadLetterExchange) {
-        return BindingBuilder.bind(parserToReducerDeadLetterQueue).to(deadLetterExchange);
-    }
+  @Bean
+  public Binding parserToReduceDeadLetterBinding(
+      Queue parserToReducerDeadLetterQueue,
+      DirectExchange deadLetterExchange,
+      RabbitDeadLetterProperties rabbitDeadLetterProperties) {
+    return BindingBuilder.bind(parserToReducerDeadLetterQueue).to(deadLetterExchange)
+        .with(rabbitDeadLetterProperties.getParserToReducerRoutingKey());
+  }
 
-    @Bean
-    public Binding webToParserDeadLetterBinding(
-            Queue webToParserDeadLetterQueue,
-            FanoutExchange deadLetterExchange) {
-        return BindingBuilder.bind(webToParserDeadLetterQueue).to(deadLetterExchange);
-    }
+  @Bean
+  public Binding webToParserDeadLetterBinding(
+      Queue webToParserDeadLetterQueue,
+      DirectExchange deadLetterExchange,
+      RabbitDeadLetterProperties rabbitDeadLetterProperties) {
+    return BindingBuilder.bind(webToParserDeadLetterQueue).to(deadLetterExchange)
+        .with(rabbitDeadLetterProperties.getWebToParserRoutingKey());
+  }
 
 }
