aa49b62f26e31e0ab3e49bc69c9c2cbdc97ffd13
Ryan Gross <ryangross47@gmail.com>
2025-09-17 16:28:50 -0400

    Lint: Add some autowired annotations

--- database/src/main/java/less/lgeo/service/ModelService.java
+++ database/src/main/java/less/lgeo/service/ModelService.java
@@ -1,132 +1,152 @@
 package less.lgeo.service;
 
-import less.lgeo.embedded.VertexEmbeddable;
-import less.lgeo.entity.*;
-import less.lgeo.primitive.*;
-import less.lgeo.repository.ColorRepository;
-import less.lgeo.repository.ModelRepository;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Service;
-
 import java.util.List;
 import java.util.Map;
 import java.util.NoSuchElementException;
 import java.util.Set;
 import java.util.function.Function;
 import java.util.stream.Collectors;
+import less.lgeo.embedded.VertexEmbeddable;
+import less.lgeo.entity.ColorEntity;
+import less.lgeo.entity.LineEntity;
+import less.lgeo.entity.ModelEntity;
+import less.lgeo.entity.OptionalLineEntity;
+import less.lgeo.entity.QuadrilateralEntity;
+import less.lgeo.entity.TriangleEntity;
+import less.lgeo.primitive.Line;
+import less.lgeo.primitive.Model;
+import less.lgeo.primitive.OptionalLine;
+import less.lgeo.primitive.Quadrilateral;
+import less.lgeo.primitive.Triangle;
+import less.lgeo.repository.ColorRepository;
+import less.lgeo.repository.ModelRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 /**
- * Fetches {@link less.lgeo.entity.ModelEntity} from the database
+ * Service layer for interacting with {@link less.lgeo.entity.ModelEntity} from the database
  */
 @Service
 public class ModelService {
 
-    private static final Logger logger = LoggerFactory.getLogger(ModelService.class);
+  private static final Logger logger = LoggerFactory.getLogger(ModelService.class);
 
-    private final ModelRepository modelRepository;
-    private final ColorRepository colorRepository;
+  @Autowired
+  private final ModelRepository modelRepository;
 
-    public ModelService(
-            ModelRepository modelRepository,
-            ColorRepository colorRepository) {
-        this.modelRepository = modelRepository;
-        this.colorRepository = colorRepository;
-    }
+  @Autowired
+  private final ColorRepository colorRepository;
 
-    /**
-     * Convert a GPB Model to a ModelEntity recursively.
-     */
-    private ModelEntity createModelEntity(Model gpb, ModelEntity parent, Map<Integer, ColorEntity> colorEntityMap) {
-        ModelEntity entity = new ModelEntity();
-        entity.setLines(gpb.getLineList().stream().map(line -> createLineEntity(entity, line, colorEntityMap)).collect(Collectors.toUnmodifiableSet()));
-        entity.setTriangles(
-                gpb.getTriangleList().stream().map(triangle -> createTriangleEntity(entity, triangle, colorEntityMap))
-                        .collect(Collectors.toUnmodifiableSet()));
-        entity.setQuadrilaterals(
-                gpb.getQuadrilateralList().stream()
-                        .map(quadrilateral -> createQuadrilateralEntity(entity, quadrilateral, colorEntityMap)).collect(Collectors.toUnmodifiableSet()));
-        entity.setOptionalLines(
-                gpb.getOptionalLineList().stream()
-                        .map(optionalLine -> createOptionalLineEntity(entity, optionalLine, colorEntityMap)).collect(Collectors.toUnmodifiableSet()));
-        entity.setParent(parent);
+  public ModelService(
+      ModelRepository modelRepository,
+      ColorRepository colorRepository) {
+    this.modelRepository = modelRepository;
+    this.colorRepository = colorRepository;
+  }
 
-        Set<ModelEntity> childEntities = gpb.getPieceList().stream()
-                .map(subModelRef -> createModelEntity(subModelRef.getSubModel(), entity, colorEntityMap))
-                .collect(Collectors.toUnmodifiableSet());
+  /**
+   * Convert a GPB Model to a ModelEntity recursively.
+   */
+  private ModelEntity createModelEntity(Model gpb, ModelEntity parent,
+      Map<Integer, ColorEntity> colorEntityMap) {
+    ModelEntity entity = new ModelEntity();
+    entity.setLines(
+        gpb.getLineList().stream().map(line -> createLineEntity(entity, line, colorEntityMap))
+            .collect(Collectors.toUnmodifiableSet()));
+    entity.setTriangles(
+        gpb.getTriangleList().stream()
+            .map(triangle -> createTriangleEntity(entity, triangle, colorEntityMap))
+            .collect(Collectors.toUnmodifiableSet()));
+    entity.setQuadrilaterals(
+        gpb.getQuadrilateralList().stream()
+            .map(quadrilateral -> createQuadrilateralEntity(entity, quadrilateral, colorEntityMap))
+            .collect(Collectors.toUnmodifiableSet()));
+    entity.setOptionalLines(
+        gpb.getOptionalLineList().stream()
+            .map(optionalLine -> createOptionalLineEntity(entity, optionalLine, colorEntityMap))
+            .collect(Collectors.toUnmodifiableSet()));
+    entity.setParent(parent);
 
-        entity.setChildren(childEntities);
+    Set<ModelEntity> childEntities = gpb.getPieceList().stream()
+        .map(subModelRef -> createModelEntity(subModelRef.getSubModel(), entity, colorEntityMap))
+        .collect(Collectors.toUnmodifiableSet());
 
-        return entity;
-    }
+    entity.setChildren(childEntities);
 
-
-    private LineEntity createLineEntity(ModelEntity parentModelEntity, Line gpb, Map<Integer, ColorEntity> colorEntityMap) {
-        LineEntity lineEntity = new LineEntity();
-        lineEntity.setModel(parentModelEntity);
-        lineEntity.setColor(colorEntityMap.get(gpb.getColorId()));
-        lineEntity.setP1(new VertexEmbeddable(gpb.getP1()));
-        lineEntity.setP2(new VertexEmbeddable(gpb.getP2()));
-        return lineEntity;
-    }
-
-    private OptionalLineEntity createOptionalLineEntity(ModelEntity parentModelEntity,
-                                                        OptionalLine gpb, Map<Integer, ColorEntity> colorEntityMap) {
-
-        OptionalLineEntity optionalLineEntity = new OptionalLineEntity();
-        optionalLineEntity.setModel(parentModelEntity);
-        optionalLineEntity.setColor(colorEntityMap.get(gpb.getColorId()));
-        optionalLineEntity.setP1(new VertexEmbeddable(gpb.getP1()));
-        optionalLineEntity.setP2(new VertexEmbeddable(gpb.getP2()));
-        optionalLineEntity.setP3(new VertexEmbeddable(gpb.getP3()));
-        optionalLineEntity.setP4(new VertexEmbeddable(gpb.getP4()));
-        return optionalLineEntity;
-    }
-
-    private QuadrilateralEntity createQuadrilateralEntity(ModelEntity parentModelEntity,
-                                                          Quadrilateral gpb, Map<Integer, ColorEntity> colorEntityMap) {
-        QuadrilateralEntity quadrilateralEntity = new QuadrilateralEntity();
-        quadrilateralEntity.setModel(parentModelEntity);
-        quadrilateralEntity.setColor(colorEntityMap.get(gpb.getColorId()));
-        quadrilateralEntity.setP1(new VertexEmbeddable(gpb.getP1()));
-        quadrilateralEntity.setP2(new VertexEmbeddable(gpb.getP2()));
-        quadrilateralEntity.setP3(new VertexEmbeddable(gpb.getP3()));
-        quadrilateralEntity.setP4(new VertexEmbeddable(gpb.getP4()));
-        return quadrilateralEntity;
-    }
+    return entity;
+  }
 
 
-    private TriangleEntity createTriangleEntity(ModelEntity parentModelEntity, Triangle gpb, Map<Integer, ColorEntity> colorEntityMap) {
-        TriangleEntity triangleEntity = new TriangleEntity();
-        triangleEntity.setModel(parentModelEntity);
-        triangleEntity.setColor(colorEntityMap.get(gpb.getColorId()));
-        triangleEntity.setP1(new VertexEmbeddable(gpb.getP1()));
-        triangleEntity.setP2(new VertexEmbeddable(gpb.getP2()));
-        triangleEntity.setP3(new VertexEmbeddable(gpb.getP3()));
-        return triangleEntity;
-    }
+  private LineEntity createLineEntity(ModelEntity parentModelEntity, Line gpb,
+      Map<Integer, ColorEntity> colorEntityMap) {
+    LineEntity lineEntity = new LineEntity();
+    lineEntity.setModel(parentModelEntity);
+    lineEntity.setColor(colorEntityMap.get(gpb.getColorId()));
+    lineEntity.setP1(new VertexEmbeddable(gpb.getP1()));
+    lineEntity.setP2(new VertexEmbeddable(gpb.getP2()));
+    return lineEntity;
+  }
+
+  private OptionalLineEntity createOptionalLineEntity(ModelEntity parentModelEntity,
+      OptionalLine gpb, Map<Integer, ColorEntity> colorEntityMap) {
+
+    OptionalLineEntity optionalLineEntity = new OptionalLineEntity();
+    optionalLineEntity.setModel(parentModelEntity);
+    optionalLineEntity.setColor(colorEntityMap.get(gpb.getColorId()));
+    optionalLineEntity.setP1(new VertexEmbeddable(gpb.getP1()));
+    optionalLineEntity.setP2(new VertexEmbeddable(gpb.getP2()));
+    optionalLineEntity.setP3(new VertexEmbeddable(gpb.getP3()));
+    optionalLineEntity.setP4(new VertexEmbeddable(gpb.getP4()));
+    return optionalLineEntity;
+  }
+
+  private QuadrilateralEntity createQuadrilateralEntity(ModelEntity parentModelEntity,
+      Quadrilateral gpb, Map<Integer, ColorEntity> colorEntityMap) {
+    QuadrilateralEntity quadrilateralEntity = new QuadrilateralEntity();
+    quadrilateralEntity.setModel(parentModelEntity);
+    quadrilateralEntity.setColor(colorEntityMap.get(gpb.getColorId()));
+    quadrilateralEntity.setP1(new VertexEmbeddable(gpb.getP1()));
+    quadrilateralEntity.setP2(new VertexEmbeddable(gpb.getP2()));
+    quadrilateralEntity.setP3(new VertexEmbeddable(gpb.getP3()));
+    quadrilateralEntity.setP4(new VertexEmbeddable(gpb.getP4()));
+    return quadrilateralEntity;
+  }
 
 
-    /**
-     * @return database entity by Model Id throws if not found
-     */
-    public ModelEntity getModelById(Long id) throws NoSuchElementException {
-        return modelRepository.findById(id).orElseThrow();
-    }
+  private TriangleEntity createTriangleEntity(ModelEntity parentModelEntity, Triangle gpb,
+      Map<Integer, ColorEntity> colorEntityMap) {
+    TriangleEntity triangleEntity = new TriangleEntity();
+    triangleEntity.setModel(parentModelEntity);
+    triangleEntity.setColor(colorEntityMap.get(gpb.getColorId()));
+    triangleEntity.setP1(new VertexEmbeddable(gpb.getP1()));
+    triangleEntity.setP2(new VertexEmbeddable(gpb.getP2()));
+    triangleEntity.setP3(new VertexEmbeddable(gpb.getP3()));
+    return triangleEntity;
+  }
 
-    public Long insertModel(Model model) {
-        Map<Integer, ColorEntity> colorEntityMap = colorRepository.findAll().stream().collect(
-                Collectors.toMap(
-                        ColorEntity::getId,
-                        Function.identity()
-                )
-        );
 
-        return modelRepository.save(createModelEntity(model, null, colorEntityMap)).getId();
-    }
+  /**
+   * @return database entity by Model Id throws if not found
+   */
+  public ModelEntity getModelById(Long id) throws NoSuchElementException {
+    return modelRepository.findById(id).orElseThrow();
+  }
 
-    public List<Long> getAllParentModelIds() {
-        return modelRepository.findAllParentModelIds();
-    }
+  public Long insertModel(Model model) {
+    Map<Integer, ColorEntity> colorEntityMap = colorRepository.findAll().stream().collect(
+        Collectors.toMap(
+            ColorEntity::getId,
+            Function.identity()
+        )
+    );
+
+    return modelRepository.save(createModelEntity(model, null, colorEntityMap)).getId();
+  }
+
+  public List<Long> getAllParentModelIds() {
+    return modelRepository.findAllParentModelIds();
+  }
 
 }
--- parser/src/main/java/less/lgeo/consumer/ParserConsumer.java
+++ parser/src/main/java/less/lgeo/consumer/ParserConsumer.java
@@ -6,6 +6,7 @@ import less.lgeo.messaging.ModelJobRequest;
 import less.lgeo.rabbitmq.properties.RabbitWebToParserRpcProperties;
 import org.springframework.amqp.rabbit.annotation.RabbitHandler;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
@@ -13,21 +14,24 @@ import org.springframework.stereotype.Component;
 @RabbitListener(queues = "#{rabbitWebToParserRpcProperties.getName()}")
 public class ParserConsumer {
 
-    private final ParserHandler parserHandler;
+  @Autowired
+  private final ParserHandler parserHandler;
 
-    //Needed for Spring Expression Language(SpEL) to get queueName
-    private final RabbitWebToParserRpcProperties rabbitWebToParserRpcProperties;
+  //Needed for Spring Expression Language(SpEL) to get queueName
+  @Autowired
+  private final RabbitWebToParserRpcProperties rabbitWebToParserRpcProperties;
 
 
-    public ParserConsumer(ParserHandler parserHandler, RabbitWebToParserRpcProperties rabbitWebToParserRpcProperties) {
-        this.parserHandler = parserHandler;
-        this.rabbitWebToParserRpcProperties = rabbitWebToParserRpcProperties;
-    }
+  public ParserConsumer(ParserHandler parserHandler,
+      RabbitWebToParserRpcProperties rabbitWebToParserRpcProperties) {
+    this.parserHandler = parserHandler;
+    this.rabbitWebToParserRpcProperties = rabbitWebToParserRpcProperties;
+  }
 
-    @RabbitHandler
-    public Long handleMessage(@Payload byte[] modelJobRequest)
-            throws InvalidProtocolBufferException {
-        return parserHandler.consume(ModelJobRequest.parseFrom(modelJobRequest));
-    }
+  @RabbitHandler
+  public Long handleMessage(@Payload byte[] modelJobRequest)
+      throws InvalidProtocolBufferException {
+    return parserHandler.consume(ModelJobRequest.parseFrom(modelJobRequest));
+  }
 
 }
