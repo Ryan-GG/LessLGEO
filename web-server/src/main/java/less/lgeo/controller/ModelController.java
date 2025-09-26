32d3de5ffa567d8fd581aa10eb111eef8ec8bcc1
Ryan Gross <ryangross47@gmail.com>
2025-09-25 22:19:14 -0400

    Refactor & Lint: Add ModelController API Test

--- web-server/src/main/java/less/lgeo/controller/ModelController.java
+++ web-server/src/main/java/less/lgeo/controller/ModelController.java
@@ -1,5 +1,7 @@
 package less.lgeo.controller;
 
+import java.util.List;
+import java.util.NoSuchElementException;
 import less.lgeo.entity.ModelEntity;
 import less.lgeo.producer.WebServerProducer;
 import less.lgeo.service.ModelService;
@@ -8,10 +10,12 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-import java.util.NoSuchElementException;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * REST API endpoints for {@link ModelController} CRUD operations
@@ -21,37 +25,37 @@ import java.util.NoSuchElementException;
 @RequestMapping(value = "/v1/models")
 public class ModelController {
 
-    private static final Logger logger = LoggerFactory.getLogger(ModelController.class);
-    @Autowired
-    private final WebServerProducer webServerProducer;
+  private static final Logger logger = LoggerFactory.getLogger(ModelController.class);
+  @Autowired
+  private final WebServerProducer webServerProducer;
 
-    @Autowired
-    private final ModelService modelService;
+  @Autowired
+  private final ModelService modelService;
 
-    public ModelController(WebServerProducer webServerProducer, ModelService modelService) {
-        this.webServerProducer = webServerProducer;
-        this.modelService = modelService;
+  public ModelController(WebServerProducer webServerProducer, ModelService modelService) {
+    this.webServerProducer = webServerProducer;
+    this.modelService = modelService;
+  }
+
+  @PostMapping("/insert")
+  public ResponseEntity<Long> insertModel(@RequestBody String body) {
+    Long modelId = webServerProducer.sendMessage(body);
+    return ResponseEntity.ok(modelId);
+  }
+
+  @GetMapping(value = "/{id}")
+  public ResponseEntity<ModelEntity> getModel(@PathVariable long id) {
+    try {
+      ModelEntity modelEntity = modelService.getModelById(id);
+      return ResponseEntity.ok(modelEntity);
+    } catch (NoSuchElementException e) {
+      logger.error("ModelEntity Id {} was not found", id);
+      return ResponseEntity.internalServerError().body(new ModelEntity());
     }
+  }
 
-    @PostMapping("/insert")
-    public ResponseEntity<Long> insertModel(@RequestBody String body) {
-        Long modelId = webServerProducer.sendMessage(body);
-        return ResponseEntity.ok(modelId);
-    }
-
-    @GetMapping(value = "/{id}")
-    public ResponseEntity<ModelEntity> getModel(@PathVariable long id) {
-        try {
-            ModelEntity modelEntity = modelService.getModelById(id);
-            return ResponseEntity.ok(modelEntity);
-        } catch (NoSuchElementException e) {
-            logger.error("ModelEntity Id {} was not found", id);
-            return ResponseEntity.internalServerError().body(new ModelEntity());
-        }
-    }
-
-    @GetMapping("/ids")
-    public ResponseEntity<List<Long>> getAllModelIds() {
-        return ResponseEntity.ok(modelService.getAllParentModelIds());
-    }
+  @GetMapping("/ids")
+  public ResponseEntity<List<Long>> getAllParentModelIds() {
+    return ResponseEntity.ok(modelService.getAllParentModelIds());
+  }
 }
--- web-server/src/test/java/less/lgeo/ModelControllerTest.java
+++ web-server/src/test/java/less/lgeo/ModelControllerTest.java
@@ -1,73 +1,90 @@
-package less.lgeo;/*
 package less.lgeo;
-
-import less.lgeo.controller.ModelController;
-import less.lgeo.service.ModelService;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
-import org.springframework.test.web.servlet.MockMvc;
 
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@AutoConfigureMockMvc
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.protobuf.util.JsonFormat;
+import java.util.List;
+import less.lgeo.controller.ModelController;
+import less.lgeo.embedded.VertexEmbeddable;
+import less.lgeo.entity.ColorEntity;
+import less.lgeo.entity.LineEntity;
+import less.lgeo.entity.ModelEntity;
+import less.lgeo.primitive.Model;
+import less.lgeo.producer.WebServerProducer;
+import less.lgeo.service.ModelService;
+import less.lgeo.test.ModelTestUtils;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+/**
+ * API Controller test for models, use a WebMVCTest to setup a mock API server. We can make mock
+ * calls to these end points and return back expected values. THe ObjectMapper allows for conversion
+ * from object type to JSON which endpoints return `content()` as.
+ */
+@Nested
 @WebMvcTest(ModelController.class)
 class ModelControllerTest {
 
-    @Autowired
-    private MockMvc mockMvc;
+  @Autowired
+  private ObjectMapper objectMapper;
+  @Autowired
+  private MockMvc mockMvc;
+  @MockitoBean
+  private ModelService modelService;
+  @MockitoBean
+  private WebServerProducer webServerProducer;
 
-    @MockitoBean
-    private ModelService modelService;
+  @Test
+  void insert() throws Exception {
+    Model cube = ModelTestUtils.cube().build();
 
-    @Test
-    void greetingShouldReturnMessageFromService() throws Exception {
-        when(modelService.getModelById(1L)).thenReturn(null);
-        this.mockMvc.perform(get("/greeting"))
-                .andDo(print())
-                .andExpect(
-                        status().isOk()
-                )
-                .andExpect(
-                        content().string("foo")
-                );
-    }
+    String cubeJson = JsonFormat.printer().print(cube);
+    when(webServerProducer.sendMessage(cubeJson)).thenReturn(1L);
+
+    mockMvc.perform(post("/v1/models/insert")
+            .contentType("application/json")
+            .content(cubeJson))
+        .andExpect(status().isOk())
+        .andExpect(content().string("1"));
+  }
+
+  @Test
+  void getModel() throws Exception {
+
+    VertexEmbeddable p1 = new VertexEmbeddable(0, 0, 0);
+    VertexEmbeddable p2 = new VertexEmbeddable(1, 1, 1);
+    ColorEntity colorEntity = new ColorEntity(1, "Black", "ffffff", false, 0, 0, 0, 0);
+    ModelEntity modelEntity = new ModelEntity();
+    modelEntity.setId(1L);
+    modelEntity.setParent(null);
+    LineEntity lineEntity = new LineEntity(1L, modelEntity, colorEntity, p1, p2);
+    modelEntity.setLines(List.of(lineEntity));
+    when(modelService.getModelById(1L)).thenReturn(modelEntity);
+
+    mockMvc.perform(get("/v1/models/{id}", 1)
+            .contentType("application/json"))
+        .andExpect(status().isOk())
+        .andExpect(content().string(objectMapper.writeValueAsString(modelEntity)));
+  }
+
+  @Test
+  void getAllParentModelIds() throws Exception {
+    List<Long> mockedReturn = List.of(1L, 2L, 3L, 4L);
+    when(modelService.getAllParentModelIds()).thenReturn(mockedReturn);
+    mockMvc.perform(get("/v1/models/ids")
+            .contentType("application/json"))
+        .andExpect(status().isOk())
+        .andExpect(content().string(objectMapper.writeValueAsString(mockedReturn)));
+  }
 }
-*/
 
-import less.lgeo.entity.ModelEntity;
-import less.lgeo.repository.ModelRepository;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.context.ContextConfiguration;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-
-@DataJpaTest
-@ExtendWith(TestContainersInitializer.class)
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-@ContextConfiguration(initializers = TestContainersInitializer.class)
-public class ModelControllerTest {
-    @Autowired
-    private ModelRepository repository;
-
-    @Test
-    void givenTestcontainersPostgres_whenSavePerson_thenSavedEntityShouldBeReturnedWithExpectedFields() {
-        ModelEntity person = new ModelEntity();
-        person.setId(1L);
-
-        ModelEntity savedPerson = repository.save(person);
-        assertNotNull(savedPerson.getId());
-    }
-}
