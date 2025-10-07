1c42b270b433290338446aa2213912c90dce2aa4
RyanGross <ryangross47@gmail.com>
2025-10-06 22:19:31 -0400

    Task: Writing unit test for Boundingbox

--- reducer/src/test/java/less/lgeo/tracer/BoundBoxTest.java
+++ reducer/src/test/java/less/lgeo/tracer/BoundBoxTest.java
@@ -10,10 +10,30 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 import java.util.Set;
 
+import static less.lgeo.common.VertexUtils.toVertex;
 import static less.lgeo.primitive.QuadrilateralUtils.getVertices;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class BoundBoxTest {
+
+
+    @Test
+    void getBoundingBoxSinglePoint() {
+        Vertex vertex = toVertex(1, 1, 1);
+        BoundingBox boundingBox = new BoundingBox(List.of(vertex));
+        assertTrue(boundingBox.includesPoint(vertex));
+    }
+
+    @Test
+    void getBoundingBoxThreePoints() {
+        Vertex min = toVertex(0, 0, 0);
+        Vertex mid = toVertex(0.5, -0.5, -0.5);
+        Vertex max = toVertex(1, -1, 1);
+        BoundingBox boundingBox = new BoundingBox(List.of(min, mid, max));
+        assertTrue(boundingBox.includesPoint(min));
+        assertTrue(boundingBox.includesPoint(mid));
+        assertTrue(boundingBox.includesPoint(max));
+    }
 
     @Test
     void getBoundBoxQuads() {
