c6a9f0750b99ed5d3449c8a5cb96e1342596e833
RyanGross <ryangross47@gmail.com>
2025-10-11 22:43:19 -0400

    Bugfix: Fix bounding box returning a mutable reference to min/max and center rather than returning a copy of the vector3

--- reducer/src/main/java/less/lgeo/tracer/BoundingBox.java
+++ reducer/src/main/java/less/lgeo/tracer/BoundingBox.java
@@ -1,12 +1,5 @@
 package less.lgeo.tracer;
 
-import static less.lgeo.common.VertexUtils.toVector3d;
-import static less.lgeo.common.VertexUtils.toVertex;
-import static less.lgeo.primitive.LineUtils.toLine;
-import static less.lgeo.test.ModelTestUtils.UNKNOWN_COLOR_ID;
-
-import java.util.List;
-import java.util.stream.Stream;
 import less.lgeo.common.Vertex;
 import less.lgeo.primitive.Line;
 import less.lgeo.primitive.Triangle;
@@ -14,8 +7,17 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.joml.Vector3d;
 
+import java.util.List;
+import java.util.stream.Stream;
+
+import static less.lgeo.common.VertexUtils.toVector3d;
+import static less.lgeo.common.VertexUtils.toVertex;
+import static less.lgeo.primitive.LineUtils.toLine;
+import static less.lgeo.test.ModelTestUtils.UNKNOWN_COLOR_ID;
+
 /**
  * Represents a 3D axis-aligned box defined by its 8 vertices.
+ * A bounding box has no reference to LDraw Orientation, just numerically gets min/max
  * <p>
  * Coordinate system:
  * <ul>
@@ -64,162 +66,147 @@ import org.joml.Vector3d;
 @NoArgsConstructor
 public class BoundingBox {
 
-  private Vector3d min = new Vector3d(Double.POSITIVE_INFINITY, Double.NEGATIVE_INFINITY,
-      Double.POSITIVE_INFINITY);
-  private Vector3d max = new Vector3d(Double.NEGATIVE_INFINITY, Double.POSITIVE_INFINITY,
-      Double.NEGATIVE_INFINITY);
+    private final Vector3d min = new Vector3d(Double.POSITIVE_INFINITY);
+    private final Vector3d max = new Vector3d(Double.NEGATIVE_INFINITY);
 
-  public BoundingBox(List<Vertex> vertices) {
-    vertices.forEach(this::growToInclude);
-  }
+    public BoundingBox(List<Vertex> vertices) {
+        vertices.forEach(this::growToInclude);
+    }
 
-  private Vector3d calculateA() {
-    return new Vector3d(min.x, max.y, min.z);
-  }
+    private Vector3d calculateA() {
+        return min;
+    }
 
-  private Vector3d calculateB() {
-    return new Vector3d(max.x, max.y, min.z);
-  }
+    private Vector3d calculateB() {
+        return new Vector3d(max.x, min.y, min.z);
+    }
 
-  private Vector3d calculateC() {
-    return max;
-  }
+    private Vector3d calculateC() {
+        return new Vector3d(max.x, min.y, max.z);
+    }
 
-  private Vector3d calculateD() {
-    return new Vector3d(min.x, max.y, max.z);
-  }
+    private Vector3d calculateD() {
+        return new Vector3d(min.x, min.y, max.z);
+    }
 
-  private Vector3d calculateE() {
-    return min;
-  }
+    private Vector3d calculateE() {
+        return new Vector3d(min.x, max.y, min.z);
+    }
 
-  private Vector3d calculateF() {
-    return new Vector3d(max.x, min.y, min.z);
-  }
+    private Vector3d calculateF() {
+        return new Vector3d(max.x, max.y, min.z);
+    }
 
-  private Vector3d calculateG() {
-    return new Vector3d(max.x, min.y, max.z);
-  }
+    private Vector3d calculateG() {
+        return max;
+    }
 
-  private Vector3d calculateH() {
-    return new Vector3d(min.x(), min.y, max.z);
-  }
+    private Vector3d calculateH() {
+        return new Vector3d(min.x(), max.y, max.z);
+    }
 
-  public void growToInclude(Vertex point) {
-    growToInclude(toVector3d(point));
-  }
+    public void growToInclude(Vertex point) {
+        growToInclude(toVector3d(point));
+    }
 
-  public void growToInclude(Vector3d point) {
-    min = min(min, point);
-    max = max(max, point);
-  }
+    public void growToInclude(Vector3d point) {
+        min.min(point);
+        max.max(point);
+    }
 
-  private Vector3d min(Vector3d a, Vector3d b) {
-    double xMin = Math.min(a.x, b.x);
-    double yMin = Math.max(a.y, b.y);
-    double zMin = Math.min(a.z, b.z);
-    return new Vector3d(xMin, yMin, zMin);
-  }
+    public void growToInclude(Triangle triangle) {
+        growToInclude(triangle.getP1());
+        growToInclude(triangle.getP2());
+        growToInclude(triangle.getP3());
+    }
 
-  private Vector3d max(Vector3d a, Vector3d b) {
-    double xMax = Math.max(a.x, b.x);
-    double yMax = Math.min(a.y, b.y);
-    double zMax = Math.max(a.z, b.z);
-    return new Vector3d(xMax, yMax, zMax);
-  }
+    public boolean includesPoint(Vector3d point) {
+        boolean inXBounds = min.x <= point.x && point.x <= max.x;
+        boolean inYBounds = min.y <= point.y && point.y <= max.y;
+        boolean inZBounds = min.z <= point.z && point.z <= max.z;
 
-  public void growToInclude(Triangle triangle) {
-    growToInclude(triangle.getP1());
-    growToInclude(triangle.getP2());
-    growToInclude(triangle.getP3());
-  }
+        return inXBounds && inYBounds && inZBounds;
+    }
 
-  public boolean includesPoint(Vector3d point) {
-    boolean inXBounds = min.x <= point.x && point.x <= max.x;
-    // Flipped due to -Y being UP
-    boolean inYBounds = max.y <= point.y && point.y <= min.y;
-    boolean inZBounds = min.z <= point.z && point.z <= max.z;
+    public boolean includesPoint(Vertex point) {
+        return includesPoint(toVector3d(point));
+    }
 
-    return inXBounds && inYBounds && inZBounds;
-  }
+    public Vector3d getCenter() {
+        Vector3d min = new Vector3d(calculateA());
+        Vector3d max = new Vector3d(calculateG());
+        return min.add(max).mul(0.5);
+    }
 
-  public boolean includesPoint(Vertex point) {
-    return includesPoint(toVector3d(point));
-  }
+    public List<Line> getBoundingBoxAsLines() {
+        return Stream.of(
+                        getTopFace(),
+                        getBottomFace(),
+                        getTopToBottomConnectingLines())
+                .flatMap(List::stream)
+                .toList();
 
-  public Vector3d getCenter() {
-    return (getMin().add(getMax())).mul(0.5);
-  }
+    }
 
-  public List<Line> getBoundingBoxAsLines() {
-    return Stream.of(
-            getTopFace(),
-            getBottomFace(),
-            getTopToBottomConnectingLines())
-        .flatMap(List::stream)
-        .toList();
+    private List<Line> getTopFace() {
+        Vector3d a = calculateA();
+        Vector3d b = calculateB();
+        Vector3d c = calculateC();
+        Vector3d d = calculateD();
+        return List.of(
+                toLine(UNKNOWN_COLOR_ID, toVertex(a), toVertex(b)),
+                toLine(UNKNOWN_COLOR_ID, toVertex(b), toVertex(c)),
+                toLine(UNKNOWN_COLOR_ID, toVertex(c), toVertex(d)),
+                toLine(UNKNOWN_COLOR_ID, toVertex(d), toVertex(a))
+        );
+    }
 
-  }
+    private List<Line> getBottomFace() {
+        Vector3d e = calculateE();
+        Vector3d f = calculateF();
+        Vector3d g = calculateG();
+        Vector3d h = calculateH();
+        return List.of(
+                toLine(UNKNOWN_COLOR_ID, toVertex(e), toVertex(f)),
+                toLine(UNKNOWN_COLOR_ID, toVertex(f), toVertex(g)),
+                toLine(UNKNOWN_COLOR_ID, toVertex(g), toVertex(h)),
+                toLine(UNKNOWN_COLOR_ID, toVertex(h), toVertex(e))
+        );
+    }
 
-  private List<Line> getTopFace() {
-    Vector3d a = calculateA();
-    Vector3d b = calculateB();
-    Vector3d c = calculateC();
-    Vector3d d = calculateD();
-    return List.of(
-        toLine(UNKNOWN_COLOR_ID, toVertex(a), toVertex(b)),
-        toLine(UNKNOWN_COLOR_ID, toVertex(b), toVertex(c)),
-        toLine(UNKNOWN_COLOR_ID, toVertex(c), toVertex(d)),
-        toLine(UNKNOWN_COLOR_ID, toVertex(d), toVertex(a))
-    );
-  }
+    /**
+     * <p>
+     * a -> e, b -> f, c -> g, d -> h
+     * </p>
+     *
+     * @return Connecting lines between top and bottom faces of the bounding box
+     */
+    private List<Line> getTopToBottomConnectingLines() {
+        Vector3d a = calculateA();
+        Vector3d e = calculateE();
+        Vector3d b = calculateB();
+        Vector3d f = calculateF();
+        Vector3d c = calculateC();
+        Vector3d g = calculateG();
+        Vector3d d = calculateD();
+        Vector3d h = calculateH();
 
-  private List<Line> getBottomFace() {
-    Vector3d e = calculateE();
-    Vector3d f = calculateF();
-    Vector3d g = calculateG();
-    Vector3d h = calculateH();
-    return List.of(
-        toLine(UNKNOWN_COLOR_ID, toVertex(e), toVertex(f)),
-        toLine(UNKNOWN_COLOR_ID, toVertex(f), toVertex(g)),
-        toLine(UNKNOWN_COLOR_ID, toVertex(g), toVertex(h)),
-        toLine(UNKNOWN_COLOR_ID, toVertex(h), toVertex(e))
-    );
-  }
+        return List.of(
+                toLine(UNKNOWN_COLOR_ID, toVertex(a), toVertex(e)),
+                toLine(UNKNOWN_COLOR_ID, toVertex(b), toVertex(f)),
+                toLine(UNKNOWN_COLOR_ID, toVertex(c), toVertex(g)),
+                toLine(UNKNOWN_COLOR_ID, toVertex(d), toVertex(h))
+        );
+    }
 
-  /**
-   * <p>
-   * a -> e, b -> f, c -> g, d -> h
-   * </p>
-   *
-   * @return Connecting lines between top and bottom faces of the bounding box
-   */
-  private List<Line> getTopToBottomConnectingLines() {
-    Vector3d a = calculateA();
-    Vector3d e = calculateE();
-    Vector3d b = calculateB();
-    Vector3d f = calculateF();
-    Vector3d c = calculateC();
-    Vector3d g = calculateG();
-    Vector3d d = calculateD();
-    Vector3d h = calculateH();
-
-    return List.of(
-        toLine(UNKNOWN_COLOR_ID, toVertex(a), toVertex(e)),
-        toLine(UNKNOWN_COLOR_ID, toVertex(b), toVertex(f)),
-        toLine(UNKNOWN_COLOR_ID, toVertex(c), toVertex(g)),
-        toLine(UNKNOWN_COLOR_ID, toVertex(d), toVertex(h))
-    );
-  }
-
-  @Override
-  public String toString() {
-    return String.format("Min: %s, Center: %s, Max: %s",
-        getMin(),
-        getCenter(),
-        getMax()
-    );
-  }
+    @Override
+    public String toString() {
+        return String.format("Min: %s, Center: %s, Max: %s",
+                getMin(),
+                getCenter(),
+                getMax()
+        );
+    }
 
 
 }
--- reducer/src/test/java/less/lgeo/tracer/BoundingVolumeHierarchyTest.java
+++ reducer/src/test/java/less/lgeo/tracer/BoundingVolumeHierarchyTest.java
@@ -1,9 +1,5 @@
 package less.lgeo.tracer;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-
-import java.util.List;
 import less.lgeo.primitive.Model;
 import less.lgeo.primitive.ModelUtils;
 import less.lgeo.primitive.Triangle;
@@ -11,27 +7,43 @@ import less.lgeo.test.ModelTestUtils;
 import org.joml.Vector3d;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 public class BoundingVolumeHierarchyTest {
 
-  @Test
-  void testBVHConstructionAndBoundingBoxes() {
+    /**
+     * For a cube, each quadrilateral will be split into two triangles and become both each a child from the parent node.
+     * Since each triangle extends to the min and max of the polygon, all values should be the same for the bounding box.
+     * (i.e. - cube made oof triangles )
+     */
+    @Test
+    void testBVHWithSharedPoints() {
 
-    Model cube = ModelTestUtils.cube().build();
-    //Tessellation is fine
-    List<Triangle> triangles = ModelUtils.tessellateModel(cube);
+        Model cube = ModelTestUtils.cube().build();
+        List<Triangle> triangles = ModelUtils.tessellateModel(cube);
 
-    // Create BVH
-    BoundingVolumeHierarchy bvh = new BoundingVolumeHierarchy(triangles);
+        BoundingVolumeHierarchy bvh = new BoundingVolumeHierarchy(triangles);
 
-    // Root should not be null
-    assertNotNull(bvh.getRoot(), "Root node should not be null");
+        BoundingVolumeHierarchy.Node root = bvh.getRoot();
+        assertNotNull(root, "Root node should not be null");
+        assertNotNull(root.getBoundingBox(), "Root bounding box should not be null");
 
-    // Root bounding box should not be null
-    assertNotNull(bvh.getRoot().getBoundingBox(), "Root bounding box should not be null");
+        BoundingBox rootBoundingBox = root.getBoundingBox();
+        assertEquals(new Vector3d(0, -1, 0), rootBoundingBox.getMin());
+        assertEquals(new Vector3d(0.5, -0.5, 0.5), rootBoundingBox.getCenter());
+        assertEquals(new Vector3d(1, 0, 1), rootBoundingBox.getMax());
 
-    BoundingBox rootBoundingBox = bvh.getRoot().getBoundingBox();
-    assertEquals(new Vector3d(0, 0, 0), rootBoundingBox.getMin());
-    assertEquals(new Vector3d(0.5, -0.5, 0.5), rootBoundingBox.getCenter());
-    assertEquals(new Vector3d(1, -1, 1), rootBoundingBox.getMin());
-  }
+        BoundingBox childABoundingBox = root.getChildA().getBoundingBox();
+        assertEquals(new Vector3d(0, -1, 0), childABoundingBox.getMin());
+        assertEquals(new Vector3d(0.5, -0.5, 0.5), childABoundingBox.getCenter());
+        assertEquals(new Vector3d(1, 0, 1), childABoundingBox.getMax());
+
+        BoundingBox childBBoundingBox = root.getChildB().getBoundingBox();
+        assertEquals(new Vector3d(0, -1, 0), childBBoundingBox.getMin());
+        assertEquals(new Vector3d(0.5, -0.5, 0.5), childBBoundingBox.getCenter());
+        assertEquals(new Vector3d(1, 0, 1), childBBoundingBox.getMax());
+    }
 }
