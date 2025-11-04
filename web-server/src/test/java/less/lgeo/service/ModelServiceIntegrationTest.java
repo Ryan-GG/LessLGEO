0503160cd981c691232ba2d78808bccd724aa0ce
Ryan Gross <ryangross47@gmail.com>
2025-11-04 17:56:50 -0500

    Task: Switch to Zonky library for postgres tests

--- web-server/src/test/java/less/lgeo/service/ModelServiceIntegrationTest.java
+++ web-server/src/test/java/less/lgeo/service/ModelServiceIntegrationTest.java
@@ -1,15 +1,15 @@
 package less.lgeo.service;
 
-import less.lgeo.PostgresTestInitializer;
 import less.lgeo.embedded.ModelId;
 import less.lgeo.entity.ModelEntity;
 import less.lgeo.primitive.Model;
 import less.lgeo.test.ModelTestUtils;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ContextConfiguration;
+
+import io.zonky.test.db.AutoConfigureEmbeddedDatabase;
+import io.zonky.test.db.AutoConfigureEmbeddedDatabase.RefreshMode;
 
 import java.util.List;
 
@@ -18,47 +18,48 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 /**
  * <p>
- * Corresponds to Integration to {@link ModelService} rather than having the test in Database,
- * Spring needs access to an Application Context respective to a main class. In this case the
- * {@link less.lgeo.WebServer} is the main interaction with the database layer and thus will hold
+ * Corresponds to Integration to {@link ModelService} rather than having the
+ * test in Database,
+ * Spring needs access to an Application Context respective to a main class. In
+ * this case the
+ * {@link less.lgeo.WebServer} is the main interaction with the database layer
+ * and thus will hold
  * integration tests for it.
  * </p>
  * <p>
- * `application-test.yaml` configures setup for Spring bean creation and allows us to make
- * modification to logging / configuration of tests. This should be kept in sync with the main
+ * `application-test.yaml` configures setup for Spring bean creation and allows
+ * us to make
+ * modification to logging / configuration of tests. This should be kept in sync
+ * with the main
  * `application.yaml`
  * </p>
  *
  * @author Ryan Gross
  */
-@SpringBootTest(
-        classes = less.lgeo.WebServer.class,
-        properties = "spring.config.location=classpath:/application-test.yaml"
-)
-@ExtendWith(value = {PostgresTestInitializer.class})
-@ContextConfiguration(initializers = PostgresTestInitializer.class)
+@SpringBootTest(classes = less.lgeo.WebServer.class, properties = "spring.config.location=classpath:/application-test.yaml")
+@AutoConfigureEmbeddedDatabase(refresh = RefreshMode.AFTER_EACH_TEST_METHOD)
 public class ModelServiceIntegrationTest {
 
-    @Autowired
-    private ModelService modelService;
+  @Autowired
+  private ModelService modelService;
 
-    @Test
-    void saveAndRetrieveModel() {
+  @Test
+  void saveAndRetrieveModel() {
 
-        Model model = ModelTestUtils.cube();
-        ModelId id = modelService.insertModel(model);
+    Model model = ModelTestUtils.cube();
+    ModelId id = modelService.insertModel(model);
 
-        ModelEntity retrievedEntity = modelService.getModelById(id).orElseThrow();
-        assertNotNull(retrievedEntity);
-        assertEquals(6, retrievedEntity.getQuadrilaterals().size());
-    }
+    ModelEntity retrievedEntity = modelService.getModelById(id).orElseThrow();
+    assertNotNull(retrievedEntity);
+    assertEquals(6, retrievedEntity.getQuadrilaterals().size());
+  }
 
-    @Test
-    void getParentModelIds() {
-        ModelId cubeId = modelService.insertModel(ModelTestUtils.cube());
-        ModelId nestedCubesId = modelService.insertModel(ModelTestUtils.nestedCubes());
+  @Test
+  void getParentModelIds() {
+    ModelId cubeId = modelService.insertModel(ModelTestUtils.cube());
+    ModelId nestedCubesId = modelService.insertModel(ModelTestUtils.nestedCubes());
 
-        assertEquals(List.of(cubeId, nestedCubesId), modelService.getAllRootModelIds());
-    }
+    assertEquals(List.of(cubeId, nestedCubesId), modelService.getAllRootModelIds());
+  }
 
 }
