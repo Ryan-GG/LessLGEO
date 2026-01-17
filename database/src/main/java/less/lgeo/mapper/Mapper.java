f562be30aec06b871e19562d656056b8227f3c7e
RyanGross <ryangross47@gmail.com>
2026-01-17 18:05:57 -0500

    Doc: Add javadoc to Mapper interface

--- database/src/main/java/less/lgeo/mapper/Mapper.java
+++ database/src/main/java/less/lgeo/mapper/Mapper.java
@@ -2,6 +2,12 @@ package less.lgeo.mapper;
 
 import java.util.List;
 
+/**
+ * Defines mapping logic between the domain model(POJO) and a database entity object
+ *
+ * @param <Domain> POJO, See - {@link less.lgeo.primitive.Quadrilateral}, {@link less.lgeo.primitive.Triangle}
+ * @param <Entity> Database object, See -  {@link less.lgeo.entity.QuadrilateralEntity},{@link less.lgeo.entity.TriangleEntity}
+ */
 public interface Mapper<Domain, Entity> {
 
     Domain toDomain(Entity entity);
