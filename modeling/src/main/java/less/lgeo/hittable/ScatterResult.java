dd76775e9682eaf831557b56ba057831bd7c6fdf
RyanGross <ryangross47@gmail.com>
2026-01-25 15:47:41 -0500

    Doc: Remove fixme

--- modeling/src/main/java/less/lgeo/hittable/ScatterResult.java
+++ modeling/src/main/java/less/lgeo/hittable/ScatterResult.java
@@ -3,7 +3,5 @@ package less.lgeo.hittable;
 import less.lgeo.common.Ray;
 import org.joml.Vector3d;
 
-//FIXME, fix for mutation reference but could cleanup more
-//fixme, should just return optional for the isScattered boolean
 public record ScatterResult(Vector3d attenuation, Ray scatteredRay) {
 }
